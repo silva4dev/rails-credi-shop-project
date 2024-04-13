8222caf9030b2690239b281986b9d1a5196168b6
silva4dev <web4dev.lucas@outlook.com>
2024-04-13 01:50:49 -0300

    feat: add channels

---
+++ app/modules/proponents/infrastructure/channels/application_cable/connection.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Proponents
+  module Infrastructure
+    module Channels
+      module ApplicationCable
+        class Connection < ActionCable::Connection::Base
+        end
+      end
+    end
+  end
+end
