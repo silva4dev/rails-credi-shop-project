d26432dc14cff9924414c1e91348a429f603a17a
silva4dev <web4dev.lucas@outlook.com>
2024-04-13 01:52:23 -0300

    feat: add proponents repository

---
+++ app/modules/proponents/infrastructure/repositories/proponents_repository.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../domain/repositories/proponents_repository_if'
+
+module Proponents
+  module Infrastructure
+    module Repositories
+      class ProponentsRepository < Domain::Repositories::ProponentsRepositoryIF
+      end
+    end
+  end
+end
