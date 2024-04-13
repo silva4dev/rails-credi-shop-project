bbde5f6403075079479aa8ff2366a65198c00430
silva4dev <web4dev.lucas@outlook.com>
2024-04-13 04:28:47 -0300

    feat: add create proponent command

---
+++ app/modules/proponents/application/commands/create_proponent_command.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Proponents
+  module Application
+    module Commands
+      class CreateProponentCommand
+        attr_reader :name, :cpf, :date_of_birth, :salary, :address, :phone
+
+        def initialize(input)
+          @name = input[:name]
+          @cpf = input[:cpf]
+          @date_of_birth = input[:date_of_birth]
+          @salary = input[:salary]
+          @address = input[:address_attributes]
+          @phone = input[:phone_attributes]
+        end
+      end
+    end
+  end
+end
