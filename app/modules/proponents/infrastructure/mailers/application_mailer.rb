137e13f3a36b6c6078b8379cd40fd134803b47f4
silva4dev <web4dev.lucas@outlook.com>
2024-04-13 01:51:25 -0300

    feat: add application mailer

---
+++ app/modules/proponents/infrastructure/mailers/application_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Proponents
+  module Infrastructure
+    module Mailers
+      class ApplicationMailer < ActionMailer::Base
+        default from: "from@example.com"
+        layout "mailer"
+      end
+    end
+  end
+end
