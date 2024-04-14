816c54dd9775046788c9c6ad6f58c14a705e0415
silva4dev <web4dev.lucas@outlook.com>
2024-04-14 04:31:57 -0300

    feat: add proponent model

--- app/modules/proponents/infrastructure/models/proponent.rb
+++ app/modules/proponents/infrastructure/models/proponent.rb
@@ -6,16 +6,21 @@ module Proponents
   module Infrastructure
     module Models
       class Proponent < ApplicationRecord
+        self.table_name = 'proponents'
+
         has_one :address, dependent: :destroy
-        has_many :phones, dependent: :destroy, as: :phoneable
+        has_one :phone, dependent: :destroy
+
+        accepts_nested_attributes_for :address, :phone
 
         validates :name, :cpf, :date_of_birth, :salary, presence: true
         validates :cpf, uniqueness: { case_sensitive: false }
         validates :cpf, cpf: true, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/ }
         validates :date_of_birth, date: { before_or_equal_to: proc { Time.zone.now }, message: :invalid }
 
-        accepts_nested_attributes_for :address
-        accepts_nested_attributes_for :phones, allow_destroy: true
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'proponent')
+        end
       end
     end
   end
