7ee1d28f0f24c8194c05141484c81fde80ef02ef
silva4dev <web4dev.lucas@outlook.com>
2024-04-14 04:31:39 -0300

    feat: add address model

--- app/modules/proponents/infrastructure/models/address.rb
+++ app/modules/proponents/infrastructure/models/address.rb
@@ -6,10 +6,16 @@ module Proponents
   module Infrastructure
     module Models
       class Address < ApplicationRecord
+        self.table_name = 'addresses'
+
         belongs_to :proponent
 
-        validates :street, :number, :district, :city, :state, :cep, presence: true
-        validates :cep, format: { with: /\A\d{5}-\d{3}\z/ }
+        validates :street, :number, :district, :city, :uf, :zipcode, presence: true
+        validates :zipcode, format: { with: /\A\d{5}-\d{3}\z/ }
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'address')
+        end
       end
     end
   end
