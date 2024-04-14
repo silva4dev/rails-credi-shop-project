8e9aa314b509f5ebcc58c035c30b0b4357028f0d
silva4dev <web4dev.lucas@outlook.com>
2024-04-14 04:31:49 -0300

    feat: add phone model

--- app/modules/proponents/infrastructure/models/phone.rb
+++ app/modules/proponents/infrastructure/models/phone.rb
@@ -6,14 +6,18 @@ module Proponents
   module Infrastructure
     module Models
       class Phone < ApplicationRecord
-        enum type: { personal: 0, reference: 1 }
+        self.table_name = 'phones'
+
+        enum phone_type: { personal: 0, reference: 1 }
 
         belongs_to :proponent
 
-        validates :number, presence: true
+        validates :number, :phone_type, presence: true
         validates :number, format: { with: /\A\+55\s\(\d{2}\)\s\d{5}-\d{4}\z/ }
 
-        validates :type, inclusion: { in: %w[personal reference] }
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'phone')
+        end
       end
     end
   end
