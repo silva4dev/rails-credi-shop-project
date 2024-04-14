dd1c3939adfd39c4c1bb82d278c2b91a0524ff8c
silva4dev <web4dev.lucas@outlook.com>
2024-04-14 04:33:47 -0300

    refactor: remove configs unnecessaries

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 #
 # Uncomment this and change the path if necessary to include your own
 # components.
@@ -14,7 +15,7 @@ SimpleForm.setup do |config|
   # stack. The options given below are used to wrap the
   # whole input.
   config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
+                            hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -52,10 +53,10 @@ SimpleForm.setup do |config|
     b.optional :readonly
 
     ## Inputs
-    # b.use :input, class: 'input', error_class: 'is-invalid', valid_class: 'is-valid'
+    # b.use :input, error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+    # b.use :hint, wrap_with: { tag: :span, class: :hint }
+    # b.use :error, wrap_with: { tag: :div, class: 'invalid-feedback' }
 
     ## full_messages_for
     # If you want to display the full error message for the attribute, you can
