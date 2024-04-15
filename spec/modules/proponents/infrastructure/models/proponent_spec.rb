cd6bef3c15118005c43d1e1870b66cbef59fb89b
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 01:25:10 -0300

    test: add proponent model

---
+++ spec/modules/proponents/infrastructure/models/proponent_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Proponents::Infrastructure::Models::Proponent, type: :model do
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      proponent = build(:proponent)
+      expect(proponent).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      proponent = build(:proponent, name: nil)
+      expect(proponent).not_to be_valid
+    end
+
+    it 'is not valid without a valid CPF format' do
+      proponent = build(:proponent, cpf: '12345678900') # CPF inválido
+      expect(proponent).not_to be_valid
+    end
+
+    it 'is not valid with a CPF that already exists' do
+      existing_proponent = create(:proponent) # Cria um proponente existente
+      proponent = build(:proponent, cpf: existing_proponent.cpf)
+      expect(proponent).not_to be_valid
+    end
+
+    it 'is not valid with a date of birth in the future' do
+      proponent = build(:proponent, date_of_birth: Date.tomorrow)
+      expect(proponent).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_one(:address).dependent(:destroy) }
+    it { is_expected.to have_one(:phone).dependent(:destroy) }
+  end
+end
