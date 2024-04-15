c178812979cf5e664f0c0e15b01ac926b70fec09
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 01:23:01 -0300

    test: add destroy proponent command

---
+++ spec/modules/proponents/application/commands/destroy_proponent_command_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Proponents::Application::Commands::DestroyProponentCommand do
+  describe '#initialize' do
+    it 'initializes with the correct id' do
+      input = { id: 1 }
+      command = described_class.new(input)
+
+      expect(command.id).to eq(1)
+    end
+  end
+end
