5cfac325cef6788919076f60f10edc0c21d565d9
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:40:52 -0300

    feat: add user model

---
+++ app/modules/sessions/infrastructure/models/user.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative 'application_record'
+
+module Sessions
+  module Infrastructure
+    module Models
+      class User < ApplicationRecord
+        self.table_name = 'users'
+
+        has_secure_password
+        validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'user')
+        end
+      end
+    end
+  end
+end
