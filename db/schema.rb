6cbe402da4c39d4d746992764afca5ac5921c8df
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:42:07 -0300

    feat: add users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_043124) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_191830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_12_043124) do
     t.string "cpf"
     t.date "date_of_birth"
     t.decimal "salary", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
