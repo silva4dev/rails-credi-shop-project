2cb3c13e2e3fe8b4c63ce5c0131c83f2fcc6b86b
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:40:04 -0300

    feat: add channels

---
+++ app/modules/sessions/infrastructure/channels/application_cable/channel.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Sessions
+  module Infrastructure
+    module Channels
+      module ApplicationCable
+        class Channel < ActionCable::Channel::Base
+        end
+      end
+    end
+  end
+end
---
+++ app/modules/sessions/infrastructure/channels/application_cable/connection.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Sessions
+  module Infrastructure
+    module Channels
+      module ApplicationCable
+        class Connection < ActionCable::Connection::Base
+        end
+      end
+    end
+  end
+end
