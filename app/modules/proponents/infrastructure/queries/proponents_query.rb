5d1c1e1cbe468dbf6516bb4e3fde65ed14ffd790
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:39:19 -0300

    feat: add proponents query

--- app/modules/proponents/infrastructure/queries/proponents_query.rb
+++ app/modules/proponents/infrastructure/queries/proponents_query.rb
@@ -8,8 +8,8 @@ module Proponents
     module Queries
       class ProponentsQuery < Domain::Queries::ProponentsQueryIF
         class << self
-          def paginate_by(input)
-            Models::Proponent.page(input[:page]).per(5)
+          def find_all_proponents
+            Models::Proponent.all
           end
         end
       end
