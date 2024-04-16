2f1e060f7c900a8361e9c6e5b305d35f4ab329ae
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:41:46 -0300

    feat: add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: redirect("/proponents")
+  root to: redirect("/sign_in")
+
   scope module: :proponents do
     scope module: :application do
       scope module: :controllers do
         resources :proponents
+      end
+    end
+  end
+
+  scope module: :sessions do
+    scope module: :application do
+      scope module: :controllers do
+        get 'sign_up', to: 'registrations#new'
+        post 'sign_up', to: 'registrations#create'
+        get 'sign_in', to: 'logins#new'
+        post 'sign_in', to: 'logins#create'
+        delete 'logout', to: 'logins#destroy'
       end
     end
   end
