c788d7eae1896ada83c9acb01a1d429b7e547630
silva4dev <web4dev.lucas@outlook.com>
2024-04-15 21:39:28 -0300

    feat: add application controller

---
+++ app/modules/sessions/application/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Sessions
+  module Application
+    module Controllers
+      class ApplicationController < ActionController::Base
+        layout 'layouts/application'
+      end
+    end
+  end
+end
